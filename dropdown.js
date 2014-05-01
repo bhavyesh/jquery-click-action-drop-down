b8f47d2374fd6b7c07f8c52aaee49b789f545955
bhavyesh <bhavyesh2567@gmail.com>
2014-05-01 17:45:42 +0530

    jQuery-click-action-drop-down

---
+++ dropdown.js
@@ -0,0 +1,33 @@
+$(function () {
+    
+ $('.dropdown').hide();
+    
+    var ddshow = function(event)
+    {
+        var target = $(event.target);
+        var ddid = target.attr('data-ddid');
+        
+        if(target.hasClass('clicker') && !target.hasClass('active'))
+        {
+            ddhide();
+            $(ddid).slideDown(300);
+            $(target).addClass('active');
+            return;
+        }
+        ddhide(event);
+    }
+    
+    var ddhide = function(event)
+    {
+        var targetGroup = event ? $(event.target).parents().addBack() : null;
+                         
+        if (targetGroup && targetGroup.is('.dropdown') && !targetGroup.is('.closeit')) { return; }
+        
+        $('.dropdown').slideUp(300);
+        $('.clicker').removeClass('active');
+       
+    }
+    
+    $(document).on('click', ddshow);
+    
+});
